eabe44d3b902bc355a4b08bd1063357615b8097f
wanwan89 <iwansetwan26@gmail.com>
2026-04-21 18:26:45 +0700

    update

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,4 @@
-// =======================
-// SUPABASE INIT
-// =======================
-const SUPABASE_URL = "https://hqetnqnvmdxdgfnnluew.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxZXRucW52bWR4ZGdmbm5sdWV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MzUyODIsImV4cCI6MjA4NzMxMTI4Mn0.Cr9lDBZMqfeONi1dfyFzHpBtawBzZTQLBEWKmPJVAOA";
-
-const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+import { supabase as db } from '../lib/supabase.js';
 
 // =======================
 // MAINTENANCE SYSTEM
