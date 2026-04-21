df43f134688c483f98d0f45016bfe36c521a3ba4
wanwan89 <iwansetwan26@gmail.com>
2026-04-21 17:29:37 +0700

    update

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-
 // Ngambil key dari file .env (Astro way)
 const SUPABASE_URL = import.meta.env.PUBLIC_SUPABASE_URL;
 const SUPABASE_ANON_KEY = import.meta.env.PUBLIC_SUPABASE_ANON_KEY;
