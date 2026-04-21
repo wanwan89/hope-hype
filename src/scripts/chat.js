f1100c2ed7ad7609d37ffb8f42cc36c927743e73
wanwan89 <iwansetwan26@gmail.com>
2026-04-22 04:47:02 +0700

    update

--- src/scripts/chat.js
+++ src/scripts/chat.js
@@ -1,10 +1,6 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js";
+import { supabase } from '../lib/supabase.js';
 
-// ===== Supabase config =====
-const SUPABASE_URL = "https://hqetnqnvmdxdgfnnluew.supabase.co";
-const SUPABASE_ANON_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxZXRucW52bWR4ZGdmbm5sdWV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE3MzUyODIsImV4cCI6MjA4NzMxMTI4Mn0.Cr9lDBZMqfeONi1dfyFzHpBtawBzZTQLBEWKmPJVAOA";
-const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// ... sisa kode lu di bawahnya biarin aja
 
 // ===== Audio Config =====
 const sendSound = new Audio("asets/sound/send.mp3");
